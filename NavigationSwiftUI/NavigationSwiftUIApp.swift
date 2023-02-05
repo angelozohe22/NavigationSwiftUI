061f50cc43873b601fda455a8d6384bdf79628a2
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-02-04 22:33:22 -0500

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Luis Angel Inga Mendoza on 4/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
